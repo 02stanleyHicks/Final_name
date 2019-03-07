04f9974ed7419ffc19c43aec78f563829187570e
Colton Hicks <02staneley.hicks@gulllakecs.org>
2019-03-07 13:02:44 -0500

    Add variables kinda

--- Final thing.py
+++ Final thing.py
@@ -1,4 +1,14 @@
 #stanley Hicks
 #no flippin clue
 #3.7.19
-print('what to do.....')
+print('what to do.....')#add message to inspire
+
+
+
+
+
+
+
+
+name =input(print('What is your name?:'))
+weight= input(print('How much do you weight?:'))
