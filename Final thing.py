aa957c3321b3dfed1b2a68f223c59aa6ef89d17d
Colton Hicks <02staneley.hicks@gulllakecs.org>
2019-03-11 12:42:10 -0400

    added while loop

--- Final thing.py
+++ Final thing.py
@@ -5,11 +5,19 @@ print('what to do.....')#add message to inspire
 
 
 
-i=int(input("how many wrestlers are you asking for: "))
+i=int(input("how many wrestlers are you asking for: "))#how many
 
 
 for i in range(i):
-    name =input('What is your name?:')
+    name =input('What is your name?:')#name and weight
     weight= input('How much do you weight?:')
 
 
+while_attempt = input('if you would like to add more wresters type "yes":')
+
+if while_attempt =='yes':
+    while True:
+        name =input('What is your name?:')#name and weight
+        weight= input('How much do you weight?:')
+        stop = input('would you like to be done then type done if not type no')
+        if stop== 'done': break 
