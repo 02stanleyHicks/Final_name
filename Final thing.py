3b8c1a47bf9f49f5d56e68150eb0ebf4973ad16b
Colton Hicks <02staneley.hicks@gulllakecs.org>
2019-03-11 13:02:19 -0400

    added comments

--- Final thing.py
+++ Final thing.py
@@ -1,23 +1,23 @@
 #stanley Hicks
 #no flippin clue
 #3.7.19
-print('what to do.....')#add message to inspire
 
 
+def list():#define my list of questions
+    name = input('What is your name?:')  # name and weight
+    weight = input('How much do you weight?:')
 
 i=int(input("how many wrestlers are you asking for: "))#how many
 
 
-for i in range(i):
-    name =input('What is your name?:')#name and weight
-    weight= input('How much do you weight?:')
+for i in range(i):#do my list of questions how many times as user wants
+   list()
 
 
-while_attempt = input('if you would like to add more wresters type "yes":')
+while_attempt = input('if you would like to add more wresters type "yes":')#ask if you want to add more people to the list
 
-if while_attempt =='yes':
+if while_attempt =='yes':#if they said yes keep it going
     while True:
-        name =input('What is your name?:')#name and weight
-        weight= input('How much do you weight?:')
+        list()
         stop = input('would you like to be done then type done if not type no')
         if stop== 'done': break 
