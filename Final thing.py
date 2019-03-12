952708459408f2327debe65c8caf378d558a0720
Colton Hicks <02staneley.hicks@gulllakecs.org>
2019-03-12 13:04:47 -0400

    add arguments

--- Final thing.py
+++ Final thing.py
@@ -3,6 +3,13 @@
 #3.7.19
 
 
+def question(wrestler, weight):
+    print('what is the', wrestler ,'name and there',weight,)
+
+question('mike', 103)
+
+'''
+
 def list():#define my list of questions
     name = input('What is your name?:')  # name and weight
     weight = input('How much do you weight?:')
@@ -16,8 +23,11 @@ for i in range(i):#do my list of questions how many times as user wants
 
 while_attempt = input('if you would like to add more wresters type "yes":')#ask if you want to add more people to the list
 
+the point of the while loop is to see if the user wants to enter in something they forgot to put on the first time
+
 if while_attempt =='yes':#if they said yes keep it going
     while True:
         list()
         stop = input('would you like to be done then type done if not type no')
         if stop== 'done': break 
+'''
